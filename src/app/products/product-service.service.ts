46256ffc26c4cd3bb4a8fa0c3798ec779455dea2
= <=avishkaprabha360@gmil.com>
2023-11-17 16:47:25 +0530

    product service

--- src/app/products/product-service.service.ts
+++ src/app/products/product-service.service.ts
@@ -7,20 +7,16 @@ export class ProductServiceService {
   private myArray: any[] = [];
   constructor() {
     this.myArray = [
-      { 'id': 1, 'name': 'Tikiri Marie', 'price': 310, 'qty': 34 },
-      { 'id': 2, 'name': 'Lanka Soy 90g', 'price': 138, 'qty': 44 },
-      { 'id': 3, 'name': 'Dhal 500g', 'price': 280, 'qty': 67 },
-      { 'id': 4, 'name': 'Elephant house vanilla 1 litre', 'price': 520, 'qty': 23 },
-      { 'id': 5, 'name': 'Lux soap', 'price': 110, 'qty': 78 }
+      
     ]
   }
   getProducts() {
     return this.myArray;
   }
-  updateArray(newData: any) {
+  updateArray(newData: any): any {
     //console.log(newData)
-    this.myArray.push(newData)
-    console.log(this.myArray)
+    this.myArray.push(newData[0])
+    return (this.myArray)
 
   }
 }
