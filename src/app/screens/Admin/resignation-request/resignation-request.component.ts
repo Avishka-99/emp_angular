fddc41fdf1d3fe459a72532a1b1ec9180c0635d3
= <=avishkaprabha360@gmil.com>
2023-12-01 16:42:17 +0530

    changes

--- src/app/screens/Admin/resignation-request/resignation-request.component.ts
+++ src/app/screens/Admin/resignation-request/resignation-request.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormserviceService } from './formservice.service';
 
 @Component({
   selector: 'app-resignation-request',
@@ -9,5 +10,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './resignation-request.component.css'
 })
 export class ResignationRequestComponent {
-
+  public step: number = 1;
+  constructor(private formservice: FormserviceService) { }
+  async setStep(value:number){
+    this.step+=value;
+  }
 }
